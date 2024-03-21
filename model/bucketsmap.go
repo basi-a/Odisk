5ef02b5544bb8da1f18ca20602230f981bf99f1e
basi-a <basi-a@outlook.com>
2024-03-21 17:13:11 +0800

    add bucket map init

--- model/bucketsmap.go
+++ model/bucketsmap.go
@@ -2,17 +2,24 @@ package model
 
 import (
 	g "odisk/global"
+
+	"gorm.io/gorm"
 )
 
 type Bucketmap struct {
-	UserID	uint
+	gorm.Model
+	UserID     uint `gorm:"uniqueIndex"`
 	BucketName string
 }
 
-func (bucketmap *Bucketmap)SaveMap() error {
+func (bucketmap *Bucketmap) SaveMap() error {
 	db := g.DB
 	if err := db.Create(&bucketmap).Error; err != nil {
 		return err
 	}
 	return nil
+}
+
+func InitBucketmap() {
+	g.DB.AutoMigrate(Bucketmap{})
 }
--- model/initModel.go
+++ model/initModel.go
@@ -2,4 +2,5 @@ package model
 
 func InitModel()  {
 	InitUser()
+	InitBucketmap()
 }

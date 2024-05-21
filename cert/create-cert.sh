1a39d2fe3e9ac2ef707027c13f248c515a3e864a
basi-a <basi-a@outlook.com>
2024-05-21 13:47:35 +0800

    update ip

--- cert/create-cert.sh
+++ cert/create-cert.sh
@@ -4,7 +4,7 @@ PRIVATE_KEY_FILE_NAME="server.key"
 CRT_FILE_NAME="server.crt"
 PEM_FILE_NAME="server.pem" 
 mkcert -cert-file $CRT_FILE_NAME -key-file $PRIVATE_KEY_FILE_NAME \
-    "*.basi-a.top" localhost 127.0.0.1 ::1 172.40.20.1 172.40.20.100 172.40.20.13 172.40.20.20 172.40.20.21 172.40.20.22 172.40.20.23 minio1 minio2 minio3 minio4
+    "*.basi-a.top" localhost 127.0.0.1 ::1 172.30.20.1 172.30.20.100 172.30.20.13 172.30.20.20 172.30.20.21 172.30.20.22 172.30.20.23 minio1 minio2 minio3 minio4
 openssl pkcs12 -export -inkey $PRIVATE_KEY_FILE_NAME -in $CRT_FILE_NAME -out $P12_FILE_NAME
 #password basi1024
 openssl x509 -in $CRT_FILE_NAME -text -noout
